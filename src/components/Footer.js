91400742f5252a5e0f975b4d9a9ddd21e3190cac
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2022-10-09 23:43:17 -0300

    screen size changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
 
   return (
     <div className="bg-black w-full mx-auto">
-      <div className="flex flex-row justify-center items-center p-4 max-w-screen-2xl mx-auto w-full h-full md:w-1/2">
+      <div className="flex flex-row justify-center items-center p-4 max-w-screen-lg mx-auto w-full h-full md:w-1/2">
         {icons.map(({ id, name, img, link }) => (
           <div key={id}>
             <a href={link} target="_blank">
